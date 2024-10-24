40dabd18c823964d68cd437ddba9be554bd77f6f
vishnukumar <vishnu.n@skil.ai>
2024-10-24 09:39:58 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>movie ticket booking</h1>
+      <h2>{import.meta.env.VITE_NAME}</h2>
+    </>
+  )
+}
+
+export default App
